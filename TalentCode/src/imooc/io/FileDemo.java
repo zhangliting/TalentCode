a7986319b02e46f55d2fe7450a77a3b8b370d072
zhangliting <lz10297@GCOTDVMMW795225.nam.nsroot.net>
2016-07-06 18:08:21 +0800

    java io

--- TalentCode/src/imooc/io/FileDemo.java
+++ TalentCode/src/imooc/io/FileDemo.java
@@ -13,6 +13,7 @@ import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class FileDemo {
 
@@ -111,6 +112,7 @@ public class FileDemo {
 		ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
 		Student student2 = (Student) objectInputStream.readObject();
 		System.out.println(student2);
+		ArrayList<E>
 
 	}
 
